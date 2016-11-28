eea953a5fdc971a5ef6f4f1f46409df53f69fbb7
salouro <feliaaquino@gmail.com>
2016-11-28 15:52:48 -0200

    Melhorias
    Melhorias na estrutura de janelinhas.

--- src/view/menu/Menu.java
+++ src/view/menu/Menu.java
@@ -1,14 +1,64 @@
 package view.menu;
 
+import java.awt.Dialog;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JFrame;
 
-public class Menu extends JFrame {
+public class Menu extends JFrame implements MouseListener {
 
 	private PainelMenu painel;
+	private MenuNovo novoJogo;
 
 	public Menu() {
 		painel = new PainelMenu();
 		this.setContentPane(painel);
+		this.painel.getNovo().addMouseListener(this);
+		this.painel.getPlacar().addMouseListener(this);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		String name = e.getComponent().getName();
+
+		if (name.equalsIgnoreCase(this.painel.getNovo().getName())) {
+			this.novoJogo = new MenuNovo(this);
+			this.novoJogo.setModalityType(Dialog.ModalityType.DOCUMENT_MODAL);
+			this.novoJogo.setVisible(true);
+			this.novoJogo.setLocationRelativeTo(this);
+			this.novoJogo.setResizable(false);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		String name = e.getComponent().getName();
+
+		if (name.equalsIgnoreCase(this.painel.getNovo().getName())) {
+			this.painel.getNovo().setIcon(this.painel.getNovoHoverIcon());
+		} else if (name.equalsIgnoreCase(this.painel.getPlacar().getName())) {
+			this.painel.getPlacar().setIcon(this.painel.getPlacarHoverIcon());
+		}
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		String name = e.getComponent().getName();
+
+		if (name.equalsIgnoreCase(this.painel.getNovo().getName())) {
+			this.painel.getNovo().setIcon(this.painel.getNovoIcon());
+		} else if (name.equalsIgnoreCase(this.painel.getPlacar().getName())) {
+			this.painel.getPlacar().setIcon(this.painel.getPlacarIcon());
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
 	}
 
 }
--- src/view/menu/PainelMenu.java
+++ src/view/menu/PainelMenu.java
@@ -2,21 +2,29 @@ package view.menu;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class PainelMenu extends JPanel {
+public class PainelMenu extends JPanel{
+
 	private JLabel novo;
 	private JLabel placar;
+	private ImageIcon novoIcon = new ImageIcon(this.getClass().getResource("/img/cenario/novo1.jpg"));
+	private ImageIcon novoHoverIcon = new ImageIcon(this.getClass().getResource("/img/cenario/novo.jpg"));
+	private ImageIcon placarIcon = new ImageIcon(this.getClass().getResource("/img/cenario/placar1.jpg"));
+	private ImageIcon placarHoverIcon = new ImageIcon(this.getClass().getResource("/img/cenario/placar.jpg"));
 
 	public PainelMenu() {
 		this.setBorder(new EmptyBorder(5, 5, 5, 5));
 		this.setLayout(null);
 		this.criarMenu();
+		this.setName("cu");
 	}
 
 	public void paintComponent(Graphics g) {
@@ -28,70 +36,144 @@ public class PainelMenu extends JPanel {
 	}
 
 	public void criarMenu() {
-		ImageIcon novoIcon = new ImageIcon(this.getClass().getResource("/img/cenario/novo1.jpg"));
-		ImageIcon novoHoverIcon = new ImageIcon(this.getClass().getResource("/img/cenario/novo.jpg"));
-		ImageIcon placarIcon = new ImageIcon(this.getClass().getResource("/img/cenario/placar1.jpg"));
-		ImageIcon placarHoverIcon = new ImageIcon(this.getClass().getResource("/img/cenario/placar.jpg"));
 
-		novo = new JLabel();
-		novo.setIcon(novoIcon);
+		this.novo = new JLabel();
+		this.novo.setIcon(novoIcon);
+		this.novo.setName("novo");
 		this.add(novo);
-		novo.setBounds(177, 116, 131, 46);
+		this.novo.setBounds(177, 116, 131, 46);
 
-		placar = new JLabel();
-		placar.setIcon(placarIcon);
+		this.placar = new JLabel();
+		this.placar.setIcon(placarIcon);
+		this.placar.setName("placar");
 		this.add(placar);
-		placar.setBounds(177, 186, 139, 52);
+		this.placar.setBounds(177, 186, 139, 52);
 
-		novo.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseReleased(java.awt.event.MouseEvent arg0) {
-			}
+		/*
+		 * novo.addMouseListener(new MouseListener() {
+		 * 
+		 * @Override public void mouseReleased(java.awt.event.MouseEvent arg0) {
+		 * }
+		 * 
+		 * @Override public void mousePressed(java.awt.event.MouseEvent arg0) {
+		 * }
+		 * 
+		 * @Override public void mouseExited(java.awt.event.MouseEvent arg0) {
+		 * novo.setIcon(novoIcon); }
+		 * 
+		 * @Override public void mouseEntered(java.awt.event.MouseEvent arg0) {
+		 * novo.setIcon(novoHoverIcon); }
+		 * 
+		 * @Override public void mouseClicked(java.awt.event.MouseEvent arg0) {
+		 * System.out.println(arg0.getComponent().getName()); novoJogo = new
+		 * MenuNovo(); } });
+		 * 
+		 * placar.addMouseListener(new MouseListener() {
+		 * 
+		 * @Override public void mouseReleased(java.awt.event.MouseEvent arg0) {
+		 * }
+		 * 
+		 * @Override public void mousePressed(java.awt.event.MouseEvent arg0) {
+		 * }
+		 * 
+		 * @Override public void mouseExited(java.awt.event.MouseEvent arg0) {
+		 * placar.setIcon(placarIcon); }
+		 * 
+		 * @Override public void mouseEntered(java.awt.event.MouseEvent arg0) {
+		 * placar.setIcon(placarHoverIcon); }
+		 * 
+		 * @Override public void mouseClicked(java.awt.event.MouseEvent arg0) {
+		 * } });
+		 */
 
-			@Override
-			public void mousePressed(java.awt.event.MouseEvent arg0) {
-			}
+	}
+/*
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		String name = e.getComponent().getName();
+		
+		if (name.equalsIgnoreCase(this.novo.getName())){
+			this.setEnabled(false);
+			this.novoJogo = new MenuNovo();
+			this.novoJogo.setVisible(true);
+		}
+	}
 
-			@Override
-			public void mouseExited(java.awt.event.MouseEvent arg0) {
-				novo.setIcon(novoIcon);
-			}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		String name = e.getComponent().getName();
 
-			@Override
-			public void mouseEntered(java.awt.event.MouseEvent arg0) {
-				novo.setIcon(novoHoverIcon);
-			}
+		if (name.equalsIgnoreCase(novo.getName())) {
+			this.novo.setIcon(this.novoHoverIcon);
+		} else if (name.equalsIgnoreCase(placar.getName())) {
+			this.placar.setIcon(this.placarHoverIcon);
+		}
+	}
 
-			@Override
-			public void mouseClicked(java.awt.event.MouseEvent arg0) {
-				MenuNovo novoJogo = new MenuNovo();
-				novoJogo.setVisible(true);
-			}
-		});
+	@Override
+	public void mouseExited(MouseEvent e) {
+		String name = e.getComponent().getName();
 
-		placar.addMouseListener(new MouseListener() {
-			@Override
-			public void mouseReleased(java.awt.event.MouseEvent arg0) {
-			}
+		if (name.equalsIgnoreCase(novo.getName())) {
+			this.novo.setIcon(this.novoIcon);
+		} else if (name.equalsIgnoreCase(placar.getName())) {
+			this.placar.setIcon(this.placarIcon);
+		}
+	}
 
-			@Override
-			public void mousePressed(java.awt.event.MouseEvent arg0) {
-			}
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
 
-			@Override
-			public void mouseExited(java.awt.event.MouseEvent arg0) {
-				placar.setIcon(placarIcon);
-			}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}*/
 
-			@Override
-			public void mouseEntered(java.awt.event.MouseEvent arg0) {
-				placar.setIcon(placarHoverIcon);
-			}
+	public JLabel getNovo() {
+		return novo;
+	}
 
-			@Override
-			public void mouseClicked(java.awt.event.MouseEvent arg0) {
-			}
-		});
+	public void setNovo(JLabel novo) {
+		this.novo = novo;
+	}
 
+	public JLabel getPlacar() {
+		return placar;
+	}
+
+	public void setPlacar(JLabel placar) {
+		this.placar = placar;
+	}
+
+	public ImageIcon getNovoIcon() {
+		return novoIcon;
+	}
+
+	public void setNovoIcon(ImageIcon novoIcon) {
+		this.novoIcon = novoIcon;
+	}
+
+	public ImageIcon getNovoHoverIcon() {
+		return novoHoverIcon;
+	}
+
+	public void setNovoHoverIcon(ImageIcon novoHoverIcon) {
+		this.novoHoverIcon = novoHoverIcon;
+	}
+
+	public ImageIcon getPlacarIcon() {
+		return placarIcon;
+	}
+
+	public void setPlacarIcon(ImageIcon placarIcon) {
+		this.placarIcon = placarIcon;
+	}
+
+	public ImageIcon getPlacarHoverIcon() {
+		return placarHoverIcon;
+	}
+
+	public void setPlacarHoverIcon(ImageIcon placarHoverIcon) {
+		this.placarHoverIcon = placarHoverIcon;
 	}
 }
