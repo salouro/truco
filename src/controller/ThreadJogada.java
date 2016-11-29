f73928559fd65461903ee28164636ba8d5d9e347
ruizera <victorruizleopoldino@gmail.com>
2016-11-29 15:45:54 -0200

    asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

---
+++ src/controller/ThreadJogada.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.concurrent.Semaphore;
+
+public class ThreadJogada extends Thread{
+
+	private Semaphore semaforo;
+	
+	public ThreadJogada(Semaphore semaforo) {
+		this.semaforo = semaforo;
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		try {
+			semaforo.acquire();
+			jogada();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally {
+			semaforo.release();
+		}
+	}
+
+	private void jogada() {
+		
+	}
+}
