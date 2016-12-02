bad3b29fc941d12f437a8315e098e63958a30317
salouro <salouro@loquencio>
2016-12-02 01:11:08 -0200

    Carta jogada

---
+++ src/model/CartaJogada.java
@@ -0,0 +1,28 @@
+package model;
+
+public class CartaJogada {
+	private Carta carta;
+	private Jogador jogador;
+	
+	public CartaJogada(){
+		
+	}
+
+	public Carta getCarta() {
+		return carta;
+	}
+
+	public void setCarta(Carta carta) {
+		this.carta = carta;
+	}
+
+	public Jogador getJogador() {
+		return jogador;
+	}
+
+	public void setJogador(Jogador jogador) {
+		this.jogador = jogador;
+	}
+	
+	
+}
